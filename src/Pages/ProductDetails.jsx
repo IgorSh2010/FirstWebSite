48f516956ccbbb541a45b88544f16453b7a09d25
IgorSh2010 <igor.sh2010@gmail.com>
2025-05-20 23:18:30 +0200

    Оновлено з Sanity

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -22,8 +22,8 @@ export default function ProductDetails() {
 
   return (
   <>
-    <div className='ml-60'><Breadcrumbs /></div>
-    <div className="ml-60 bg-white/5 backdrop-blur-md p-6 max-w-3xl mx-auto">
+    <div className='md:ml-60 ml-1'><Breadcrumbs /></div>
+    <div className="md:ml-60 ml-1 bg-white/5 backdrop-blur-md p-4 max-w-3xl mx-auto">
       {product.imageUrl && (
         <a href={product.imageUrl} target="_blank" rel="noreferrer">
           <img
