58dab54d8c0d4b71479c573587f80d6efaf8cf15
IgorSh2010 <igor.sh2010@gmail.com>
2025-05-20 22:05:59 +0200

    Оновлено з Sanity

--- src/components/HeaderNew.jsx
+++ src/components/HeaderNew.jsx
@@ -65,8 +65,8 @@ const Header = () => {
         {/* Контакти */}
         <div className="text-center md:text-right">
           <div className="flex space-x-4 md:items-end md:text-right">
-            <span className="font-medium text-sm mb-1 md:mb-2">Kontakty do zamówień</span>
-            <div className="flex flex-row flex-wrap md:flex-row gap-2 items-center md:justify-end">
+            
+            <div className="flex flex-row flex-wrap md:flex-row gap-2 items-end md:justify-end">
                 <a
                 href="https://www.facebook.com/larysa.shepetko"
                 target="_blank"
